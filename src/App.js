953502de1f5d35ae1d03041754f6dc652c936086
Anil-Choudhary-1993 <choudharya335@gmail.com>
2019-12-29 18:08:21 +0530

    devtool enabled in react and new plugin added for advanced js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    state = {
+        count: 0
+    }
+    increment = () => {
+        this.setState({ count: this.state.count + 1 })
+    }
+
     render() {
         return (
             <div>
                 Hello World!
+
+                <button onClick={this.increment}>
+                    Counter {this.state.count || null}
+                </button>
             </div>
         )
     }
