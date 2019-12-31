5ab9a343f8686b545102b0a9ebca1ac74e054b1e
Anil Choudhary <anil@cloudthing.com>
2019-12-31 15:41:53 +0530

    React-App Boilerplate complete

--- src/App.js
+++ src/App.js
@@ -2,25 +2,8 @@ import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 
 class App extends Component {
-  state = {
-    count: 0
-  }
-
-  increment = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
-  decrement = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
-
   render() {
-    return (
-      <div>
-        <b>Counter</b> :: {this.state.count || null}
-        <button onClick={this.increment}> Increment </button>
-        <button onClick={this.decrement}>Decrement </button>
-      </div>
-    )
+    return <div>React App BoilerPlate</div>
   }
 }
 
